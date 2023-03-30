61e5879d0721cc7422e08c862751dce7b56c5b2d
toplmin <1433783950@qq.com>
2023-03-30 10:23:11 +0800

    人脸查询页面修改

--- src/views/Face/FaceSearch.vue
+++ src/views/Face/FaceSearch.vue
@@ -1,3 +1,67 @@
 <template>
-    <h2>人脸特征查询页面</h2>
+    <div class="searchBox">
+        <span>人员姓名</span>
+        <el-select v-model="value" class="m-2" placeholder="请输入人员姓名">
+            <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+            />
+        </el-select>
+        <el-button type="primary">查询</el-button>
+        <el-button type="primary">删除</el-button>
+    </div>
+    <div class="searchResult">
+        
+    </div>
+    <div class="faceShow">
+
+    </div>
+    <div class="featureShow">
+
+    </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+export default{
+    setup(){
+        const value = ref('')
+        const options = [
+            {
+                value: 'Option1',
+                label: 'Option1',
+            },
+            {
+                value: 'Option2',
+                label: 'Option2',
+            },
+            {
+                value: 'Option3',
+                label: 'Option3',
+            },
+            {
+                value: 'Option4',
+                label: 'Option4',
+            },
+            {
+                value: 'Option5',
+                label: 'Option5',
+            },
+        ]
+    }
+}
+</script>
+
+<style scoped>
+.searchBox{
+    border: 1px solid grey;
+}
+.searchResult{
+    border: 1px solid grey;
+}
+.faceShow{
+    border: 1px solid grey;
+}
+</style>
